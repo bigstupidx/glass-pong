1c55ef5076e34b09d53c9c0f70b64219072e362c
Oded Welgreen <oded_wel@hotmail.com>
2015-03-27 13:47:07 +0300

    Paddle AI

---
+++ glass-pong/Assets/Scripts/AiPaddle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class AiPaddle : MonoBehaviour
+{
+    public float Speed = 30;   
+
+    private GameObject ball;
+    private Rigidbody2D ballBody;
+
+	// Use this for initialization
+	void Start () {
+	    ball = GameObject.Find("Ball");
+	    ballBody = ball.GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (ballBody.velocity.x > 0) return;
+
+        var diff = ball.transform.position.y - transform.position.y;
+	    if (Math.Abs(diff) < GetComponent<SpriteRenderer>().bounds.size.y/2) return;
+	    GetComponent<Rigidbody2D>().velocity = new Vector2(0,(diff < 0 ? -1 : 1)*Speed);
+
+	}
+}
---
+++ glass-pong/Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPaddle : MonoBehaviour {
+    public float Speed = 30;   
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        var v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * Speed;
+	}
+}
